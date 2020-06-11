8af0345592212456f07df58af21ad8cb08c3b10d
Justin Hamilton <JHamilton@Pas.com>
2020-06-11 09:49:09 -0500

    Add project files.

---
+++ Models/IData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CakeStoreApi.Models
+{
+    public interface IData
+    {
+        public List<CakeStore> CakesList { get; set; }
+        public List<CakeStore> CakesInitializeData();
+        public CakeStore GetCakeById(int? id);
+    }
+}
